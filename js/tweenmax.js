fb56fcf47d622da8008437dc0890a88d797d2980
sexfat <bryant6831@gmail.com>
2019-04-12 15:50:31 +0800

    add class

--- js/tweenmax.js
+++ js/tweenmax.js
@@ -5,7 +5,7 @@ TweenMax.to('.box_01', 1.4, {
     //   scaleY : 1.5,
     width: '100px',
     className: '+=vivi',
-    boxShadow : "2px 1px 10px blue"
+    boxShadow: "2px 1px 10px blue"
     // height: '400px',
     // backgroundColor: '#f20'
 
@@ -42,8 +42,12 @@ TweenMax.to('.box_01', 1.4, {
 
 
 
-TweenMax.set('.box_06' , {x: 50 });
-TweenMax.set('.box_07' , {x: 150 });
+TweenMax.set('.box_06', {
+    x: 50
+});
+TweenMax.set('.box_07', {
+    x: 150
+});
 
 
 
@@ -55,9 +59,9 @@ function alerts() {
 
 var tl = new TimelineMax({
     //方法
-   repeat: 1,
-   yoyo: true,
-   onComplete: alerts
+    repeat: 1,
+    yoyo: true,
+    onComplete: alerts
 });
 
 // var tl_01 = TweenMax.to('.box_06' , 1 ,{ x: 100});
@@ -68,9 +72,16 @@ var tl = new TimelineMax({
 
 
 
-tl.to('.box_06' , 1 ,{ x: 200})
-.to('.box_07' , 1 ,{ y: 100})
-.to('.box_08' , 1 ,{ y: 100 , x: 100});
+tl.to('.box_06', 1, {
+        x: 200
+    })
+    .to('.box_07', 1, {
+        y: 100
+    })
+    .to('.box_08', 1, {
+        y: 100,
+        x: 100
+    });
 
 tl.stop();
 
@@ -84,15 +95,101 @@ document.getElementById('btn_stop').onclick = function () {
     tl.stop();
 }
 
+var tlc = new TimelineMax({
+    onComplete: parallaxs
+});
+
+
+// tlc.to('.box_09', 1 , {left: 100}).to('.box_10', 1 , {top: 50}).to('.box_11', 1 , {right: 100}).to('.box_12', 1 , {top: 20})
 
 
 function parallaxs() {
     var scene = document.getElementById('parallax_box');
     var parallax = new Parallax(scene);
+
 };
 
 
-parallaxs();
+var controller = new ScrollMagic.Controller();
+
+
+var animation_01 = TweenMax.to('.box_13', 1, {
+    y: 100,
+    rotationX: 360
+})
+
+
+var section_01 = new ScrollMagic.Scene({
+    triggerElement: "#trigger_01",
+
+}).setTween(animation_01)
+.addIndicators()
+.addTo(controller)
+
+
+
+//第二段動畫
+var animation_02 = TweenMax.staggerFromTo('.item', 1 , {
+    y: 0,
+    alpha: 0
+},{
+    y: 30,
+    alpha: 1
+},.2)
+
+//觸發事件
+
+var section_02 = new ScrollMagic.Scene({
+    triggerElement: "#trigger_02",
+    reverse: true,
+    offset: 0,
+    // triggerHook: 0
+
+}).setTween(animation_02)
+.addIndicators()
+.addTo(controller)
+
+// 觸發事件
+
+var section_03 = new ScrollMagic.Scene({
+    triggerElement: "#trigger_03",
+    // reverse: true,
+    // offset: 0,
+    // triggerHook: 0
+
+}).setClassToggle('.box_select' , 'select')
+.addIndicators()
+.addTo(controller)
+
+
+
+
+
+
+
+
+var section_04 = new ScrollMagic.Scene({
+    triggerElement: "#trigger_04",
+    // duration: '80%',
+    // offset: 40,
+    reverse: false,
+    triggerHook: 0
+}).setClassToggle('.bgall' ,'on')//選取器 選取物件  
+.addIndicators({
+    name: 'section02'
+}).addTo(controller)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
